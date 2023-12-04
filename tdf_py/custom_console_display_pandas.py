017c51eccb68c1a92a26f500ca596ba96d7258c6
michael gugliotti <128745749+googs0@users.noreply.github.com>
2023-12-04 13:04:38 -0500

    Update and rename custom_console_display.py to custom_console_display_pandas.py

--- tdf_py/custom_console_display_pandas.py
+++ tdf_py/custom_console_display_pandas.py
@@ -1,6 +1,6 @@
 from imports import np, pd
 
-# PyCharm better viewing options for dataframe (see more than 5 col)
+# Better viewing options (see more than 5 col in Pycharm)
 desired_width = 320
 pd.set_option('display.width', desired_width)
 np.set_printoptions(linewidth=desired_width)
